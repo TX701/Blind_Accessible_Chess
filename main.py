23c8d76104271b9535d39bfb9a2bf664030a3420
Rory <167032554+TX701@users.noreply.github.com>
2025-04-13 16:03:54 -0400

    added comments

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from py.game import start_display, read
+from py.game import start_display
 import py.settings as settings
 from py.chess import create_board
 import py.engine as engine
 
 def main():
-    settings.init()
-    settings.board = create_board()
-
-    start_display()
+    settings.init() # creating the board global variable
+    settings.board = create_board() # creating the board with the inital piece
+    
+    start_display() # starting the display and game loop
     
 if __name__ == "__main__":
     main()
